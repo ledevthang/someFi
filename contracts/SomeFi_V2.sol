816d46af76ed1141f1cfad03dc4041a2a8c9d620
ledevthang <ledevthang@gmail.com>
2022-08-06 16:55:51 +0700

    update init

--- contracts/SomeFi_V2.sol
+++ contracts/SomeFi_V2.sol
@@ -82,6 +82,26 @@ contract SomeFiV2 is
         _;
     }
 
+    function initialize(
+        address _usdtContractAddress,
+        address _operatorAddress,
+        address _walletBackup,
+        address _walletMain
+    ) external initializer {
+        require(_usdtContractAddress != address(0), "invalid-USDT");
+        __ERC20_init("SomeFi", "SOFI");
+        __Ownable_init();
+        _mint(msg.sender, 10000000000000000000000000);
+        tokenUSDT = IERC20Upgradeable(_usdtContractAddress);
+        operator[_operatorAddress] = true;
+        icoHasEnded = true;
+        backupWallet = _walletBackup;
+        mainWallet = _walletMain;
+        oneHundredPercent = 10000;
+        directCommissionPercentage = 5000;
+        etherValue = 1 ether;
+    }
+
     function buyICOByUSDT(
         address ref,
         bool isLeft,
